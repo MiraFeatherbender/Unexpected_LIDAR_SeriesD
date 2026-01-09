70bf20e4a0d977445a7593cf806828dd8a4f51f1
MiraFeatherbender <jonathangorum813@gmail.com>
2026-01-09 17:23:40 -0600

    added mdns support for easy local http access

--- main/plugins/io_usb_cdc_msc.c
+++ main/plugins/io_usb_cdc_msc.c
@@ -245,7 +245,7 @@ void io_usb_cdc_msc_init(void)
     // --- USB CDC/MSC and tasks are disabled for testing. ---
     
     
-
+/*
     ESP_LOGI(TAG, "USB Composite initialization");
     const tinyusb_config_t tusb_cfg = TINYUSB_DEFAULT_CONFIG();
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
@@ -275,6 +275,6 @@ void io_usb_cdc_msc_init(void)
     dispatcher_register_handler(TARGET_USB_CDC, io_usb_dispatcher_handler);
 
     ESP_LOGI(TAG, "USB Composite initialization DONE");
-    
+    */
 }
 
--- main/plugins/io_wifi_ap.c
+++ main/plugins/io_wifi_ap.c
@@ -5,43 +5,94 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "wifi_http_server.h"
+#include "mdns.h"
 
-void io_wifi_ap_init(void)
+#include "esp_netif.h"
+
+#define MAX_STA_RETRIES 5
+static int s_retry_num = 0;
+static bool s_sta_connected = false;
+
+static void start_ap_mode(void);
+
+static void start_mdns(void) {
+    static bool mdns_started = false;
+    if (mdns_started) return;
+    if (mdns_init() == ESP_OK) {
+        mdns_hostname_set("AnkleBiter");
+        mdns_instance_name_set("ESP32 Web Server");
+        ESP_LOGI("io_wifi_ap", "mDNS started: http://AnkleBiter.local");
+        mdns_started = true;
+    } else {
+        ESP_LOGW("io_wifi_ap", "mDNS init failed");
+    }
+}
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                              int32_t event_id, void* event_data)
 {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < MAX_STA_RETRIES) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI("io_wifi_ap", "Retrying STA connection (%d/%d)", s_retry_num, MAX_STA_RETRIES);
+        } else {
+            ESP_LOGW("io_wifi_ap", "STA connection failed, switching to AP mode");
+            start_ap_mode();
         }
-    ESP_ERROR_CHECK(ret);
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        s_sta_connected = true;
+        s_retry_num = 0;
+        ESP_LOGI("io_wifi_ap", "Connected in STA mode");
+        start_mdns();
+        wifi_http_server_start();
+    }
+}
 
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-
+static void start_sta_mode(void) {
+    esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL));
+
+    wifi_config_t wifi_config = { 0 };
+    strncpy((char *)wifi_config.sta.ssid, CONFIG_WIFI_STA_SSID, sizeof(wifi_config.sta.ssid));
+    strncpy((char *)wifi_config.sta.password, CONFIG_WIFI_STA_PASSWORD, sizeof(wifi_config.sta.password));
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.pmf_cfg.capable = true;
+    wifi_config.sta.pmf_cfg.required = false;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI("io_wifi_ap", "Trying STA mode. SSID:%s", wifi_config.sta.ssid);
+}
+
+static void start_ap_mode(void) {
+    esp_netif_create_default_wifi_ap();
     wifi_config_t wifi_config = { 0 };
     strncpy((char *)wifi_config.ap.ssid, CONFIG_WIFI_AP_SSID, sizeof(wifi_config.ap.ssid));
     wifi_config.ap.ssid_len = strlen(CONFIG_WIFI_AP_SSID);
     wifi_config.ap.channel = CONFIG_WIFI_AP_CHANNEL;
     wifi_config.ap.max_connection = CONFIG_WIFI_AP_MAX_CONN;
 
-        // Auth mode selection
-    #if defined(CONFIG_WIFI_AUTH_OPEN)
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    #elif defined(CONFIG_WIFI_AUTH_WPA_PSK)
-        wifi_config.ap.authmode = WIFI_AUTH_WPA_PSK;
-    #elif defined(CONFIG_WIFI_AUTH_WPA2_PSK)
-        wifi_config.ap.authmode = WIFI_AUTH_WPA2_PSK;
-    #elif defined(CONFIG_WIFI_AUTH_WPA_WPA2_PSK)
-        wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
-    #else
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    #endif
+#if defined(CONFIG_WIFI_AUTH_OPEN)
+    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+#elif defined(CONFIG_WIFI_AUTH_WPA_PSK)
+    wifi_config.ap.authmode = WIFI_AUTH_WPA_PSK;
+#elif defined(CONFIG_WIFI_AUTH_WPA2_PSK)
+    wifi_config.ap.authmode = WIFI_AUTH_WPA2_PSK;
+#elif defined(CONFIG_WIFI_AUTH_WPA_WPA2_PSK)
+    wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+#else
+    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+#endif
 
-    // Set password if required
     if (wifi_config.ap.authmode != WIFI_AUTH_OPEN) {
         strncpy((char *)wifi_config.ap.password, CONFIG_WIFI_AP_PASSWORD, sizeof(wifi_config.ap.password));
     }
@@ -51,7 +102,23 @@ void io_wifi_ap_init(void)
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI("io_wifi_ap", "Wi-Fi AP started. SSID:%s channel:%d", wifi_config.ap.ssid, wifi_config.ap.channel);
-
-    // Start HTTP server
+    start_mdns();
     wifi_http_server_start();
+}
+
+void io_wifi_ap_init(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    s_retry_num = 0;
+    s_sta_connected = false;
+    start_sta_mode();
 }
